fab68663e9575327c3357e0abef18f3fc6ed211b
S1ms <shuisko@gmail.com>
2019-10-01 14:59:54 +0300

    Add files via upload
    Cannot upload whole project since Github allows only 100 files to be uploaded.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebActivatorEx;
+
+namespace MvcDemo.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Tietoa()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult Videos()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult React()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public JsonResult GetName()
+        {
+            return Json(new { name = "World from server side" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
